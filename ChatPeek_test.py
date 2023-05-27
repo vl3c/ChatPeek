eab92d6f3a8613ffd9ed21b5507f11fca86b18a4
vl3c <95963142+vl3c@users.noreply.github.com>
2023-05-27 17:51:43 +0300

    Remove link from test

--- ChatPeek_test.py
+++ ChatPeek_test.py
@@ -3,7 +3,7 @@ from ChatPeek import ChatPeek, Chat, Reply
 
 class TestChatPeek(unittest.TestCase):
     def test_chat_peek(self):
-        chat_peek = ChatPeek("https://chat.openai.com/share/c51a8a6b-e689-40fc-a90f-a39ba24074b0")  # insert your link here
+        chat_peek = ChatPeek("...")  # insert your link here
         chat = chat_peek.chat
         self.assertIsInstance(chat, Chat)
         self.assertIsNotNone(chat.title)
